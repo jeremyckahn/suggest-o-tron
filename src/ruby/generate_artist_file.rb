2a3f581706710a4bf642f0fc0ae19354c36b2121
Jeremy Kahn <jeremyckahn@gmail.com>
2012-10-28 16:18:11 -0700

    Writes artists to a file under /gen.

--- src/ruby/generate_artist_file.rb
+++ src/ruby/generate_artist_file.rb
@@ -1,4 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/itunes')
 
 iTunes = ITunes.new
-puts iTunes.artists_by_play_count.collect {|artist| artist[:name]}
+artists = iTunes.artists_by_play_count.collect {|artist| artist[:name]}
+output_filepath = \
+  File.expand_path(File.dirname(__FILE__) + '../../../gen/artists.txt')
+
+File.open(output_filepath, 'w') do |output|
+  artists.each {|artist| output.puts artist}
+end
